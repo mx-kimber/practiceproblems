010023bd676efbb7c56d0ad54de90a72b07411d3
Kimber Ogden <mx.kimber@gmail.com>
2023-03-22 15:03:08 -0500

    added #3 to exercise

--- conditionals.rb
+++ conditionals.rb
@@ -6,19 +6,28 @@
 #   elsif
 #     p -1
 #   end
+
 # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
-number = 5
-  if number < 10
-    p -1
-  elsif
-    number > 10
-    p 1
-  elsif
-    number == 10
-    p 0
-  end
+
+# number = 5
+#   if number < 10
+#     p -1
+#   elsif
+#     number > 10
+#     p 1
+#   elsif
+#     number == 10
+#     p 0
+#   end
 
 # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+number1 = 4
+number2 = 7
+if number1 && number2 < 10
+  p 1
+else
+  p 0
+end
 
 # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
 
