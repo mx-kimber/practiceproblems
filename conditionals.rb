65d3808157f07b1070da52a89d125d146fc70cb0
Kimber Ogden <mx.kimber@gmail.com>
2023-03-22 22:28:03 -0500

    completed exercise #4

--- conditionals.rb
+++ conditionals.rb
@@ -21,15 +21,24 @@
 #   end
 
 # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
-number1 = 4
-number2 = 7
-if number1 && number2 < 10
-  p 1
-else
-  p 0
-end
+
+# number1 = 4
+# number2 = 7
+# if number1 && number2 < 10
+#   p 1
+# else
+#   p 0
+# end
 
 # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+number = 10_000
+if number > 9000
+  p 1
+else
+  p -1
+end
+
 
 # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
 
