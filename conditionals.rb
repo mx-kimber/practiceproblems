46eede382b98d7c114ec2ceeede6890eaac59c8e
Kimber Ogden <mx.kimber@gmail.com>
2023-03-24 10:41:47 -0500

    completed exercise #10

--- conditionals.rb
+++ conditionals.rb
@@ -85,15 +85,28 @@
 # end
 
 # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
-number1 = 5
-number2 = -3
-if number1 < 0 && number2 > 0
-  p 1
-else
- p 0
-end
+
+# number1 = 5
+# number2 = -3
+# if number1 < 0 && number2 > 0
+#   p 1
+# else
+#  p 0
+# end
 
 # 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
 
+number = 66
+if number > 80
+  p 5
+elsif number > 60 && number < 80
+  p 4
+elsif number > 40 && number < 60
+  p 3
+elsif number > 20 && number < 40
+  p 2
+else
+  p 1
+end
 
 # SOLUTIONS: https://gist.github.com/peterxjang/aee70f966f0f725609eee89b06e8a6c0
