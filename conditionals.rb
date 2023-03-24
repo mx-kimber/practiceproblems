ce50e6531bee0f84eebfe0f3d59f815fd86d95ff
Kimber Ogden <mx.kimber@gmail.com>
2023-03-24 10:44:20 -0500

    fixed conditional line 17: changed to else

--- conditionals.rb
+++ conditionals.rb
@@ -14,7 +14,7 @@
 #   p -1
 # elsif number > 10
 #   p 1
-# elsif number == 10
+# else
 #   p 0
 # end
 
@@ -96,17 +96,17 @@
 
 # 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
 
-number = 66
-if number > 80
-  p 5
-elsif number > 60 && number < 80
-  p 4
-elsif number > 40 && number < 60
-  p 3
-elsif number > 20 && number < 40
-  p 2
-else
-  p 1
-end
+# number = 66
+# if number > 80
+#   p 5
+# elsif number > 60 && number < 80
+#   p 4
+# elsif number > 40 && number < 60
+#   p 3
+# elsif number > 20 && number < 40
+#   p 2
+# else
+#   p 1
+# end
 
 # SOLUTIONS: https://gist.github.com/peterxjang/aee70f966f0f725609eee89b06e8a6c0
