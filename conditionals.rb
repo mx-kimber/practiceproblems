4ca9d2b79a38ec72acb66209197b1691a67ffd2f
Kimber Ogden <mx.kimber@gmail.com>
2023-03-24 10:46:19 -0500

    fixed conditional #5

--- conditionals.rb
+++ conditionals.rb
@@ -41,11 +41,11 @@
 # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
 
 # number = 25
-# if number < 10 && number >= 0
+# if number < 10
 #   p 9
-# elsif number < 20 && number > 10
+# elsif number < 20
 #   p 19
-# elsif number < 30 && number > 20
+# elsif number < 30
 #   p 29
 # else
 #   p -1
