bb17eb8a885d0876a0b513764a514044ad684415
Kimber Ogden <mx.kimber@gmail.com>
2023-03-25 01:45:34 -0500

    first commit / added annotated practice problems

---
+++ arrays2.rb
@@ -0,0 +1,22 @@
+# 1. Start with an array of numbers and create a new array with each number times 3.
+
+# 2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
+
+# 3. Start with an array of strings and create a new array with each string upcased.
+
+# 4. Start with an array of numbers and create a new array with only the numbers greater than 100.
+
+# 5. Start with an array of numbers and create a new array with each number plus 7
+
+# 6. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
+
+# 7. Start with an array of strings and create a new array with each string's first letter only.
+
+# 8. Start with an array of numbers and create a new array with only the numbers less than 10.
+
+# 9. Start with an array of numbers and create a new array with each number divided by 2.
+
+# 10. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
+
+
+# SOLUTIONS: https://gist.github.com/peterxjang/9aec2e5ce455d78eae730ba82ad023d8
