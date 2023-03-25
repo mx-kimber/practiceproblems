764a667b5dab15358de0fd3b99ebe633cf055a71
Kimber Ogden <mx.kimber@gmail.com>
2023-03-25 10:02:12 -0500

    Completed #3 / lines 15-19

--- strings.rb
+++ strings.rb
@@ -6,11 +6,17 @@
 
 # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
 
-first_name = "Tazmanian"
-last_name = "Devil"
-p "#{first_name} #{last_name}"
+# first_name = "Tazmanian"
+# last_name = "Devil"
+# p "#{first_name} #{last_name}"
 
 # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+puts "Please type a word."
+word = gets.chomp.downcase
+if word == "marco"
+  p "polo!"
+end
 
 # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
 
