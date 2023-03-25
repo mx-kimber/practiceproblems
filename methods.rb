727b64dbf5fef4c6c5ef0277282cd581b1cf83a6
Kimber Ogden <mx.kimber@gmail.com>
2023-03-25 11:03:32 -0500

    completed exercise #2 / lines 9-13

--- methods.rb
+++ methods.rb
@@ -1,10 +1,16 @@
 # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
 
-puts "Please enter a word."
-word = gets.chomp
-p word.upcase
+# puts "Please enter a word."
+# word = gets.chomp
+# p word.upcase
 
 # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+puts "Please enter a number."
+number = gets.chomp.to_i
+if number > 100
+  puts "That's a big number!"
+end
 
 # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
 
