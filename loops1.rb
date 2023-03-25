dc4ebe00ee79121b0ce17e6eaa9102fbcbaa015e
Kimber Ogden <mx.kimber@gmail.com>
2023-03-25 01:41:55 -0500

    first commit / added annotated practice problems

---
+++ loops1.rb
@@ -0,0 +1,22 @@
+# 1. Write a times loop to print the numbers 1 through 10.
+
+# 2. Write a times loop that prints the word "hello" 5 times.
+
+# 3. Write a times loop that prints the numbers 10 through 1.
+
+# 4. Write a times loop that prints the numbers 0 through 15, increasing by 5 each time.
+
+# 5. Write a times loop that prints the number 9000 ten times.
+
+# 6. Write a times loop that prints the numbers 100 through 90.
+
+# 7. Write a times loop that prints the numbers 50 to 70.
+
+# 8. Write a times loop that prints the phrase "Around the world" 144 times.
+
+# 9. Write a times loop that prints the numbers 5 through 0.
+
+# 10. Write a times loop that prints the even numbers from 2 to 10.
+
+
+# SOLUTIONS: https://gist.github.com/peterxjang/716fc92bd11b759a5f6d5348126483e6
