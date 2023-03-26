5d81f0c1ac8c0d96d4be01d46bcdca36dbcbcc3b
Kimber Ogden <mx.kimber@gmail.com>
2023-03-26 16:50:29 -0500

    pulled first letter from array

--- arrays_select.rb
+++ arrays_select.rb
@@ -1,20 +1,33 @@
 #1. Start with an array of numbers and create a new array with only the numbers less than 20.
 #     For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
 
-numbers = [3, 6, 21, 10, 40]
-new_array = [ ]
-index = 0
-numbers.each do |number|
-  if number < 20
-    new_array << number
-  end
-index =- 1
-end
+# numbers = [3, 6, 21, 10, 40]
+# new_array = [ ]
+# index = 0
+# numbers.each do |number|
+#   if number < 20
+#     new_array << number
+#   end
+# index =- 1
+# end
 
-p new_array
+# p new_array
 
 #  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
 #     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
+
+array = ["wonder", "gold", "women", "lasso"]
+new_array = [ ]
+
+p array[0][0]
+index = 0
+# array.each do
+#   if array[index].chars(0) == "w"
+#     new_array << array[index]
+#   end
+#   index += 1
+# end
+# p new_array
 
 #  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
