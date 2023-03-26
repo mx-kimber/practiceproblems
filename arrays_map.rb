1f61be2847a55284e39a4dbc37e4ad57ca9d9825
Kimber Ogden <mx.kimber@gmail.com>
2023-03-26 11:44:44 -0500

    added lines 4-10

--- arrays_map.rb
+++ arrays_map.rb
@@ -1,6 +1,14 @@
 #  1. Start with an array of numbers and create a new array with each number times 3.
 #     For example, [1, 2, 3] becomes [3, 6, 9].
 
+numbers = [3, 6, 4]
+new_array = [ ]
+
+numbers.each do |number|
+ new_array << number * 3
+end
+p new_array
+
 #  2. Start with an array of strings and create a new array with each string upcased.
 #     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
 
