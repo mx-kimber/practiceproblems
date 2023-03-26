9bf0ac781a0c28e64668f86fddd0a9b56ffa5188
Kimber Ogden <mx.kimber@gmail.com>
2023-03-26 01:20:57 -0500

    completed lines 45-50y

--- hashes.rb
+++ hashes.rb
@@ -42,11 +42,13 @@
 
 # 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
 
-books = [
-  { },
-  { },
-  { }
+graphic_novels = [
+  {title: "Tank Girl", author: " Hewlitt/Martin"},
+  {title: "Saga", author: "Fiona Staples"},
+  {title: "Transmetropolitan", author: "Ellis/Robertson"}
 ]
+p graphic_novels[2][:author]
+
 
 # 6. Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the result.
 
