27c46ed13673fb8c95211015f514877ceafac451
Kimber Ogden <mx.kimber@gmail.com>
2023-03-26 10:33:16 -0500

    added lines 17-20

--- functions.rb
+++ functions.rb
@@ -7,12 +7,17 @@
 
 # 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
 
-def all_caps(string)
-  return string
-end
-p all_caps("Borg").upcase
+# def all_caps(string)
+#   return string
+# end
+# p all_caps("Borg").upcase
 
 # 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
+
+def subtracted(number1, number2)
+  return number2 - number1
+end
+p subtracted(5, 7)
 
 # 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
 
