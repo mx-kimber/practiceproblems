593d3bf734565d04ef7a0fb89af9771117a695fb
Kimber Ogden <mx.kimber@gmail.com>
2023-03-26 10:29:43 -0500

    added lines 10-13

--- functions.rb
+++ functions.rb
@@ -1,11 +1,16 @@
 # 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
 
-def double(number)
- return number * 2
-end
-p double(4)
+# def double(number)
+#  return number * 2
+# end
+# p double(4)
 
 # 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
+
+def all_caps(string)
+  return string
+end
+p all_caps("Borg").upcase
 
 # 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
 
