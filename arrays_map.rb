4f9b202013b80a39b96784481f5c0890c110ebca
Kimber Ogden <mx.kimber@gmail.com>
2023-03-26 15:06:06 -0500

    added lines 66-76

--- arrays_map.rb
+++ arrays_map.rb
@@ -50,18 +50,30 @@
 #  5. Start with an array of strings and create a new array with each string's length.
 #     For example, ["hello", "goodbye"] becomes [5, 7].
 
-strings = ["yes", "no", "maybe"]
-new_array = [ ]
+# strings = ["yes", "no", "maybe"]
+# new_array = [ ]
 
-index = 0
-while index < strings.length
-  new_array << strings[index].split('').count
-index += 1
-end
-p new_array
+# index = 0
+# while index < strings.length
+#   new_array << strings[index].split('').count
+# index += 1
+# end
+# p new_array
 
 #  6. Start with an array of hashes and create a new array of number values from each hash's :age key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
+
+hash_array = [
+  {name: "Bob", age: 42},
+  {name: "Trevor", age: 31}
+]
+new_array = [ ]
+index = 0
+hash_array.each do
+  new_array << hash_array[index][:age]
+  index =+ 1
+end
+ p new_array
 
 #  7. Start with an array of numbers and create a new array with each number divided by 2.
 #     For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
