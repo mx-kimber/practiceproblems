377d69374fa3f5f6c5b65efcbc4107cc462db492
Kimber Ogden <mx.kimber@gmail.com>
2023-03-26 10:16:45 -0500

    wrote lines 3-6 and had to reference notes

--- functions.rb
+++ functions.rb
@@ -1,5 +1,10 @@
 # 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
 
+def double(number)
+ return number * 2
+end
+p double(4)
+
 # 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
 
 # 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
