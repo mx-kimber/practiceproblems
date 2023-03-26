a69d8478e77230389bf81e83f3013d82c1db9959
Kimber Ogden <mx.kimber@gmail.com>
2023-03-26 11:54:17 -0500

    added lines 15-21 / used .each do

--- arrays_map.rb
+++ arrays_map.rb
@@ -1,16 +1,24 @@
 #  1. Start with an array of numbers and create a new array with each number times 3.
 #     For example, [1, 2, 3] becomes [3, 6, 9].
 
-numbers = [3, 6, 4]
-new_array = [ ]
+# numbers = [3, 6, 4]
+# new_array = [ ]
 
-numbers.each do |number|
- new_array << number * 3
-end
-p new_array
+# numbers.each do |number|
+#  new_array << number * 3
+# end
+# p new_array
 
 #  2. Start with an array of strings and create a new array with each string upcased.
 #     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
+
+strings = ["howdy", "partner"]
+cap_strings = [ ]
+
+strings.each do |string|
+  cap_strings << string.upcase
+end
+p cap_strings
 
 #  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
